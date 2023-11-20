322d9392b83934e011732cbf784e8b269290120a
Emmatosorus <emma@bouteyres.com>
2023-11-20 11:13:21 +0100

    Added Logic_diagram

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: epolitze <epolitze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 14:08:43 by epolitze          #+#    #+#             */
-/*   Updated: 2023/11/17 16:39:56 by epolitze         ###   ########.fr       */
+/*   Updated: 2023/11/20 09:56:37 by epolitze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,14 @@ char	*argument_manager(char c, void *arg)
 
 int	ft_printf(const char *str, ...)
 {
-	int		i;
-
-	i = 0;
-	while (str[i] != '%' && str[i])
-		ft_putchar_fd(str[i++], 1);
-	if (str[i++] == '%')
-	{
-		ft_putchar_fd(argument_manager(str[i], va_arg(ar, (void *)), 1);
-	}
-	return (0);
+	
 }
 
 int	main(int argc, char **argv)
 {
 	while (argc-- > 1)
 	{
-		ft_printf(argv[argc]);
+		ft_printf("%s", argv[argc]);
 		ft_printf("\n");
 	}
 }
