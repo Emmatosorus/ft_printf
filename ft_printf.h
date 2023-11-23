ad412cf0fd45b567245217816996b46c1fe1c08a
Emma Politzer <epolitze@z4r9p1.42lyon.fr>
2023-11-23 15:53:07 +0100

    Everything works?

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: epolitze <epolitze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 14:10:13 by epolitze          #+#    #+#             */
-/*   Updated: 2023/11/23 10:04:15 by epolitze         ###   ########.fr       */
+/*   Updated: 2023/11/23 13:07:38 by epolitze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
-# include <stddef.h>
-# include <stdio.h> // SUPPRIME CETTE LIGNE !!!!!!!!!!!!!!!!!!!!
 
 int		ft_printf(const char *str, ...);
 int		ft_putchar(char c);
