78811abe9406ea98619e111a49c668221efafb46
Emma Politzer <epolitze@z4r9p1.42lyon.fr>
2023-11-23 11:40:51 +0100

    Simplified printf + fixed bugs

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,11 @@
 /*   By: epolitze <epolitze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 14:08:43 by epolitze          #+#    #+#             */
-/*   Updated: 2023/11/23 10:32:09 by epolitze         ###   ########.fr       */
+/*   Updated: 2023/11/23 10:52:45 by epolitze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static int	ft_charcmp(char c, char	*str)
-{
-	int	i;
-
-	if (!c)
-		return (1);
-	else
-	{
-		i = 0;
-		while (str[i])
-		{
-			if (str[i] == c)
-				return (1);
-			i++;
-		}
-	}
-	return (0);
-}
 
 static int	null_check(void *ptr, int wcount)
 {
@@ -83,7 +64,7 @@ int	ft_printf(const char *str, ...)
 	va_start(arg, str);
 	while (str && str[++i])
 	{
-		if (str[i] == '%' && ft_charcmp(str[i + 1], "cspdiuxX%") == 1)
+		if (str[i] == '%')
 			temp = argument_manager(str[++i], &arg);
 		else
 			temp = ft_putchar(str[i]);
